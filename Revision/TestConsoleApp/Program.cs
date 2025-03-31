0b8a254fd9e8e91baf34221cb6c88a0322117593
kietta <truonganhkietdev@gmail.com>
2025-03-31 23:23:48 +0700

    add gitignore

---
+++ Revision/TestConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+StringBuilder stringBuilder = new StringBuilder();
+
+stringBuilder.Append("Hello, ");
+stringBuilder.Append("World!");
+
+Console.WriteLine(stringBuilder.ToString());
+
+string str = $"Now is: {DateTime.Now}";
+Console.WriteLine(str);
---
+++ Revision/TestProject/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Channels;
+
+namespace TestProject
+{
+    public class Class1
+    {
+        int a = 100;
+           
+    }
+}
