a7f218741c0918ca69cceefa2a144332e615e013
kietta <truonganhkietdev@gmail.com>
2025-04-02 12:45:05 +0700

    findMax, findMin

---
+++ Revision/TestConsoleApp/ArrayFromUserInput.cs
@@ -0,0 +1,61 @@
+﻿public class Practise
+{
+    public void UserInputArray()
+    {
+        Console.WriteLine("Enter the number of elements in the array");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[n];
+        Console.WriteLine("Enter the elements of the array");
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine("The elements of the array are:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(arr[i]);
+        }
+    }
+
+    public void FindMax()
+    {
+        Console.WriteLine("Enter the number of elements in the array");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[n];
+        Console.WriteLine("Enter the elements of the array");
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        int max = arr[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+        Console.WriteLine($"Max: {max}");
+    }
+
+    public void FindMin()
+    {
+        Console.WriteLine("Enter the number of elements in the array");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[n];
+        Console.WriteLine("Enter the elements of the array");
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        int min = arr[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+        }
+        Console.WriteLine($"Min: {min}");
+    }
+}
--- Revision/TestConsoleApp/Program.cs
+++ Revision/TestConsoleApp/Program.cs
@@ -1,12 +1,3 @@
-﻿int[] arr;
-Console.Write("Nhập số phần tử của mảng: ");
-int n = int.Parse(Console.ReadLine());
-
-arr = new int[n];
-for (int i = 0; i < n; i++)
-{
-    Console.Write($"Nhập phần tử thứ {i + 1}: ");
-    arr[i] = int.Parse(Console.ReadLine());
-}
-
-Console.WriteLine("Mảng vừa nhập: " + string.Join(", ", arr));
+﻿Practise practise = new Practise();
+//practise.UserInputArray();
+practise.FindMax();
